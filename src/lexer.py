486d70014e08cc62bdfefd894e97ea1334cc0ebb
-Bonfire- <40984528+BonfireScratch@users.noreply.github.com>
2019-01-23 20:07:04 +0200

    Update lexer.py

--- src/lexer.py
+++ src/lexer.py
@@ -1,25 +1,30 @@
+import varObject
 import re
 
 class Lexer(object):
     
     def __init__(self, source_code):
         self.source_code = source_code
+        self.varObj = varObject.VariableObject()
 
     def tokenize(self):
+        vars = self.varObj
         tokens = []
         source_code = self.source_code.split()
         source_index = 0
-        stringStarted = 0
+        print(vars.variables)
 
         while source_index < len(source_code):
             word = source_code[source_index]
 
             if word == "var":
                 tokens.append(["VAR_DECLARATION", word])
+            elif word in self.varObj.variables:
+                print("VARIABLE")
             elif word == "print":
                 tokens.append(["PRINT_STATEMENT", word])
-            elif re.match("[a-z]", word) or re.match("[A-Z]", word) or word[0] == "\"":             
-                if stringStarted == 0:
+            elif re.match("[a-z]", word) or re.match("[A-Z]", word) or word[0] in ["\"", "\'"]:
+                if word[0] in ["\"", "\'"]:
                     tokens.append(["STRING", word])
                 else:
                     tokens.append(["IDENTIFIER", word])
@@ -27,8 +32,6 @@ class Lexer(object):
                 tokens.append(["INTEGER", word])
             elif word in "+-*/=":
                 tokens.append(["OPERATOR", word])
-            elif word[0] == "\"":
-                stringStarted = 1 - stringStarted
 
             source_index += 1
         
