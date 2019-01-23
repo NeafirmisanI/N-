f475bd62e12c30e650b354c46d7adbcac9f3028a
-Bonfire- <40984528+BonfireScratch@users.noreply.github.com>
2019-01-23 20:06:36 +0200

    Delete varObject.py

--- src/Objects/varObject.py
+++
@@ -1,9 +0,0 @@
-class VariableObject(object):
-    
-    def __init__(self):
-        self.exec_string = ""
-        self.variables = {}
-
-    def set_variable(self, name, operator, value):
-        self.exec_string += name + " " + operator + " " + value + "\n"
-        self.variables[name] = value
