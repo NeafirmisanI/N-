452b044e262d13542dbfe30cf428b9e8178b1c3d
-Bonfire- <40984528+BonfireScratch@users.noreply.github.com>
2019-01-29 15:32:18 +0200

    Update varObject.py

--- src/varObject.py
+++ src/varObject.py
@@ -5,3 +5,6 @@ class VariableObject(object):
 
     def set_variable(self, name, value):
         self.variables[name] = value
+
+    def get_variable(self, name):
+        return self.variables[name]
