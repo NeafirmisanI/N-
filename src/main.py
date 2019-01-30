141207131138b310badb6138d502d42ee01af634
-Bonfire- <40984528+BonfireScratch@users.noreply.github.com>
2019-01-30 17:51:13 +0200

    Update main.py

--- src/main.py
+++ src/main.py
@@ -1,18 +1,29 @@
 import lexer
 import interpreter
+import sys
 
 interpret = interpreter.Parser()
 
 def main():
     print("N# Shell")
     content = ""
-    while True:
-        content = promptForInput()
-        lex = lexer.Lexer(content)
-        tokens = lex.tokenize()
-        interpret.parse(tokens)
+    if len(sys.argv) == 1:
+        promptForInput()
+    elif len(sys.argv) == 2:
+        openFile(sys.argv[1])
 
 def promptForInput():
-    return str(input("N# > "))
+     while True:
+        content = str(input("N# > "))
+        run(content)
+
+def run(code):
+     lex = lexer.Lexer(content)
+     tokens = lex.tokenize()
+     interpret.parse(tokens)
+
+def openFile(name):
+     with open(name, "r") as sourceFile:
+        run(sourceFile.read())
 
 main()
