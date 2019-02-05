4e2a7cf67a9962d4c141bc5da0b6cc881c85d3e4
-Bonfire- <40984528+BonfireScratch@users.noreply.github.com>
2019-02-05 19:20:14 +0200

    Update lexer.py

--- src/lexer.py
+++ src/lexer.py
@@ -1,103 +1,142 @@
 from error import NSError
+import string
 
-TT_PLUS = "OPERATOR"
-TT_MINUS = "OPERATOR"
-TT_MUL = "OPERATOR"
-TT_DIV = "OPERATOR"
-TT_EQU = "OPERATOR"
-TT_INT = "INT"
-TT_FLOAT = "FLOAT"
-TT_STRING = "STRING"
-TT_IDENTIFIER = "IDENTIFIER"
-TT_PRINT = "PRINT_STATEMENT"
-TT_VAR = "VAR_DECLARATION"
-DIGITS = "0123456789"
-ALPHABET = "qwertyuiopasdfghjklzxcvbnmQWERTYUIOPASDFGHJKLZXCVBNM\'\""
+class Lexer:
 
-class Lexer(object):
+    def __init__(self):
+        self.tokens= []
+        self.code = ""
+        self.pos = 0
+        self.digits = "0123456789"
 
-    def __init__(self, text):
-        self.text = text
-        self.pos = -1
-        self.current_char = None
-        self.advance()
+    def lex(self, code):
+        self.code = code
+        self.pos = 0
 
-    def advance(self):
+        while self.pos < len(self.code):
+            self.skipspace()
+            char = self.code[self.pos]
+
+            validName = (
+                string.ascii_lowercase + string.ascii_uppercase +
+                "$+-/*%=><!^"
+            )
+
+            if char in "+-*/=":
+                self.addToken("OPERATOR", char)
+                continue
+            if char in '\'\"':
+                self.string(char)
+                continue
+            if char in self.digits:
+                self.number()
+                continue
+            if (char == '-'
+                and self.current + 1 < len(self.inp)
+                and self.inp[self.current + 1] in digits):
+                        self.number()
+                        continue
+            if char in validName:
+                self.name()
+                continue
+            raise NSError("Lexical error: Unrecognized character " + char)
+        return self.tokens
+            
+    def skipspace(self):
+        while self.pos <= len(self.code):
+            if self.code[self.pos] in [' ', '\t', '\n', '\r']:
+                self.pos += 1
+                continue
+            if self.code[self.pos] == '~':
+                self.pos += 1
+                try:
+                    if self.code[self.pos] == '~':
+                        self.pos += 1
+                except IndexError:
+                    raise NSError("Uncaught SyntaxError: Invalid comment syntax")
+                self.pos += 1
+                continue
+            break
+
+    def addToken(self, typ, value):
+        self.tokens.append({
+            "type": "OPERATOR",
+            "value": value,
+        })
+        self.pos += len(str(value))
+
+    def string(self, quote):
         self.pos += 1
-        self.current_char = self.text[self.pos] if self.pos < len(self.text) else None
+        value = ""
 
-    def tokenize(self):
-        tokens = []
+        try:
+            while self.code[self.pos] != quote:
+                char = self.code[self.pos]
 
-        while self.current_char != None:
-            if self.current_char in ' \t':
-                self.advance()
-            elif self.current_char in DIGITS:
-                tokens.append(self.number())
-            elif self.current_char in ALPHABET:
-                tokens.append(self.alphabet())
-            elif self.current_char == '=':
-                tokens.append([TT_EQU, self.current_char])
-                self.advance()
-            elif self.current_char == '+':
-                tokens.append([TT_PLUS, self.current_char])
-                self.advance()
-            elif self.current_char == '-':
-                tokens.append([TT_MINUS, self.current_char])
-                self.advance()
-            elif self.current_char == '*':
-                tokens.append([TT_MUL, self.current_char])
-                self.advance()
-            elif self.current_char == '/':
-                tokens.append([TT_DIV, self.current_char])
-                self.advance()
-            else:
-                raise NSError("N# ERROR: Invalid character " + self.current_char)
+                if char == '\\':
+                    self.pos += 1
+                    if self.code[self.pos] == '"':
+                        char = '"'
+                    elif self.code[self.pos] == "'":
+                        char = "'"
+                    elif self.code[self.pos] == "\\":
+                        char = "\\"
+                    elif self.code[self.pos] == "n":
+                        char = "\n"
+                    elif self.code[self.pos] == "t":
+                        char = "\t"
+                    elif self.code[self.pos] == "b":
+                        char = "\b"
+                    elif self.code[self.pos] == "a":
+                        char = "\a"
 
-        return tokens
+                value += char
+                self.pos += 1
+        except IndexError:
+            raise NSError("Uncaught SyntaxError: Invalid string syntax")
 
+        self.pos += 1
+        self.tokens.append({
+            "type": "STRING",
+            "value": value,
+        })
+        
     def number(self):
-        num_str = ''
-        dot_count = 0
-        token = []
-
-        while self.current_char != None and self.current_char in DIGITS + '.':
-            if self.current_char == '.':
-                if dot_count == 1: break
-                dot_count += 1
-                num_str += '.'
-            else:
-                num_str += self.current_char
-            self.advance()
-
-        if dot_count == 0:
-            token = [TT_INT, int(num_str)]
+        lexeme = ""
+        while not self.atEnd() and self.code[self.pos] in self.digits:
+            lexeme += self.code[self.pos]
+            self.pos += 1
+        value = None
+        if not self.atEnd() and self.code[self.pos] == '.':
+            lexeme += self.code[self.pos]
+            self.pos += 1
+            while not self.atEnd() and self.code[self.pos] in self.digits:
+                lexeme += self.code[self.pos]
+                self.pos += 1
+            value = float(lexeme)
         else:
-            token = [TTT_FLOAT, float(num_str)]
+            value = int(lexeme)
 
-        return token
+        self.tokens.append({
+            "type": "NUMBER",
+            "value": value,
+        })
 
-    def alphabet(self):
-        alphabet_str = ''
-        quotation_count = 0
-        token = []
-
-        while self.current_char != None and self.current_char in ALPHABET + "\'\"":
-            if self.current_char in "\'\"":
-                quotation_count += 1
-                alphabet_str += self.current_char
-            else:
-                alphabet_str += self.current_char
-            self.advance()
-
-        if quotation_count == 0:
-            if alphabet_str == "print":
-                token = [TT_PRINT, alphabet_str]
-            elif alphabet_str == "var":
-                token = [TT_VAR, alphabet_str]
-            else:
-                token = [TT_IDENTIFIER, alphabet_str]
-        else:
-            token = [TT_STRING, alphabet_str]
-
-        return token
+    def name(self):
+        validName = (
+            string.ascii_lowercase + string.ascii_uppercase +
+            "$+-/*%=><!^"
+        )
+        
+        value = ""
+        while not self.atEnd() and self.code[self.pos] in validName:
+            value += self.code[self.pos]
+            self.pos += 1
+        
+        self.tokens.append({
+            "type": "IDENTIFIER",
+            "value": value,
+        })
+    
+    def atEnd(self):
+        return self.pos >= len(self.code)
