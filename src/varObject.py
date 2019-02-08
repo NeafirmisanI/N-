d3ab6afdfe8b36376361b9dc1605014d31f34d2f
-Bonfire- <40984528+BonfireScratch@users.noreply.github.com>
2019-02-08 16:53:49 +0200

    Update varObject.py

--- src/varObject.py
+++ src/varObject.py
@@ -1,7 +1,11 @@
+import platform
+import sys
+import os
+
 class VariableObject(object):
     
     def __init__(self):
-        self.variables = {}
+        self.variables = {"pi": 3.1415926535897, "euler": 2.718281828459, "os": platform.system(), "os-version": platform.release(), "argv": sys.argv, "cwd": os.getcwd(), "tau": 6.28318530718}
 
     def set_variable(self, name, value):
         self.variables[name] = value
