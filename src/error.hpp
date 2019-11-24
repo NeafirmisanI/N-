5fff5780831a3acdc0f7821b3b3c8bbcb876a553
-Bonfire- <40984528+BonfireScratch@users.noreply.github.com>
2019-11-24 16:45:35 +0200

    Update error.hpp

--- src/error.hpp
+++ src/error.hpp
@@ -1,17 +1,21 @@
+#include <algorithm>
 #include <iostream>
+#include <string>
+#include <tuple>
+#include "position.hpp"
 
 using namespace std;
 
 class Error {
     public:
-        int pos_start;
-        int pos_end;
+        Position* pos_start;
+        Position* pos_end;
         string error_name;
         string details;
         string as_string();
-        string highlight(string text, int pos_start, int pos_end);
+        string highlight(string text, Position* pos_start, Position* pos_end);
 
-        Error(int pos_s, int pos_e, string error_nam, string detail) {
+        Error(Position* pos_s, Position* pos_e, string error_nam, string detail) {
             pos_start = pos_s;
             pos_end = pos_e;
             error_name = error_nam;
