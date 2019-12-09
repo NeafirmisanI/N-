a87e2c453f57b84822e2a472d340563c18f643a3
-Bonfire- <40984528+BonfireScratch@users.noreply.github.com>
2019-12-09 20:57:35 +0200

    Update lexer.cpp

--- src/lexer.cpp
+++ src/lexer.cpp
@@ -5,87 +5,59 @@
 #include <map>
 #include "mainClasses.hpp"
 
-using namespace std;
+static std::string digits = "0123456789";
+static std::string letters = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ";
+static std::vector<std::string> KEYWORDS = {"var", "and", "or", "not", "if", "elif", "else", "for", "to", "step", "while", "def", "then", "end", "return", "continue", "break"};
 
-static string digits = "0123456789";
-static string letters = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ";
-static vector<string> KEYWORDS = {"var", "and", "or", "not", "if", "elif", "else", "for", "to", "step", "while", "def", "then", "end", "return", "continue", "break"};
-
-tuple<vector<Token>, Error> Lexer::lex() {
-    vector<Token> tokens;
+std::tuple<std::vector<Token>, Error> Lexer::lex() {
+    std::vector<Token> tokens;
 
     while (current_char != '\0') {
-        if (in(current_char, " \t")) {
-            advance();
-        } else if (current_char == '#') {
-            skip_comment();
-        } else if (in(current_char, "\n;")) {
-            tokens.push_back(Token("NEWLINE", "", pos->inx, -2));
-            advance();
-        } else if (in(current_char, digits)) {
-            tokens.push_back(parse_number());
-        } else if (in(current_char, letters)) {
-            tokens.push_back(parse_identifier());
-        } else if (current_char == '\"') {
-            tokens.push_back(parse_string());
-        } else if (current_char == '+') {
-            tokens.push_back(Token("PLUS", "\0", pos->inx, -2));
-            advance();
-        } else if (current_char == '-') {
-            tokens.push_back(Token("MINUS", "\0", pos->inx, -2));
-            advance();
-        } else if (current_char == '*') {
-            tokens.push_back(Token("MUL", "\0", pos->inx, -2));
-            advance();
-        } else if (current_char == '/') {
-            tokens.push_back(Token("DIV", "\0", pos->inx, -2));
-            advance();
-        } else if (current_char == '%') {
-            tokens.push_back(Token("MOD", "\0", pos->inx, -2));
-            advance();
-        } else if (current_char == '^') {
-            tokens.push_back(Token("POW", "\0", pos->inx, -2));
-            advance();
-        } else if (current_char == '(') {
-            tokens.push_back(Token("LPAREN", "\0", pos->inx, -2));
-            advance();
-        } else if (current_char == ')') {
-            tokens.push_back(Token("RPAREN", "\0", pos->inx, -2));
-            advance();
-        } else if (current_char == '[') {
-            tokens.push_back(Token("LSQARE", "\0", pos->inx, -2));
-            advance();
-        } else if (current_char == ']') {
-            tokens.push_back(Token("RSQARE", "\0", pos->inx, -2));
-            advance();
-        } else if (current_char == '!') {
-            tuple<Token, Error> res = parse_not_equals();
+        if (current_char == '\t' || current_char == ' ') advance();
+        if (is_identifier(current_char)) tokens.push_back(parse_identifier());
+        if (current_char == '\"') tokens.push_back(parse_string());
+        if (is_digit(current_char)) tokens.push_back(parse_number());
+        
+        switch (current_char) {
+            case '\0': break;
+            case '\n':
+            case ';': tokens.push_back(make_token("NEWLINE")); break;
+            case '+': tokens.push_back(make_token("PLUS")); break;
+            case '-': tokens.push_back(make_token("MINUS")); break;
+            case '*': tokens.push_back(make_token("MUL")); break;
+            case '/': tokens.push_back(make_token("DIV")); break;
+            case '%': tokens.push_back(make_token("MOD")); break;
+            case '^': tokens.push_back(make_token("POW")); break;
+            case '(': tokens.push_back(make_token("LPAREN")); break;
+            case ')': tokens.push_back(make_token("RPAREN")); break;
+            case '[': tokens.push_back(make_token("LSQUARE")); break;
+            case ']': tokens.push_back(make_token("RSQUARE")); break;
+            case ',': tokens.push_back(make_token("COMMA")); break;
+            case '=': tokens.push_back(parse_equals()); break;
+            case '<': tokens.push_back(parse_less_than()); break;
+            case '>': tokens.push_back(parse_greater_than()); break;
+            case '#': skip_comment();
+            case '!': {
+                    std::tuple<Token, Error> res = parse_not_equals();
 
-            if (get<1>(res).details != "") {
-                tuple<vector<Token>, Error> eres({Token("\0", "\0", -2, -2)}, get<1>(res));
-                return eres;
+                    if (std::get<1>(res).details != "") {
+                        std::tuple<std::vector<Token>, Error> eres({Token("\0", "\0", noPos, noPos)}, std::get<1>(res));
+                        return eres;
+                    }
+
+                    tokens.push_back(std::get<0>(res));
+                    break;
+            } default: {
+                    Position* pos_start = new Position(pos->inx, pos->ln, pos->col, pos->fn, pos->ftext);
+                    std::string sc(1, current_char);
+                    advance();
+                    std::tuple<std::vector<Token>, Error> eres({Token("\0", "\0", noPos, noPos)}, Error(pos_start, pos, "Illegal Character", "\'" + sc + "\'\n")); //TODO: Make token pos_start and pos_end of type Position*
+                    return eres;
             }
-
-            tokens.push_back(get<0>(res));
-        } else if (current_char == '=') {
-            tokens.push_back(parse_equals());
-        } else if (current_char == '<') {
-            tokens.push_back(parse_less_than());
-        } else if (current_char == '>') {
-            tokens.push_back(parse_greater_than());
-        } else if (current_char == ',') {
-            tokens.push_back(Token("COMMA", "\0", pos->inx, -2));
-        } else {
-            Position *pos_start = new Position(pos->inx, pos->ln, pos->col, pos->fn, pos->ftext);
-            char c = current_char;
-            string sc(1, c);
-            advance();
-            tuple<vector<Token>, Error> eres({Token("\0", "\0", -2, -2)}, Error(pos_start, pos, "Illegal Character", "\'" + sc + "\'\n"));
-            return eres;
         }
     }
 
-    tokens.push_back(Token("EOF", "\0", pos->inx, -2));
+    tokens.push_back(Token("EOF", "\0", pos, noPos));
     return make_tuple(tokens, Error(noPos, noPos, "", ""));
 }
 
@@ -104,23 +76,28 @@ void Lexer::skip_comment() {
     advance();
 }
 
-tuple<Token, Error> Lexer::parse_not_equals() {
+Token Lexer::make_token(std::string type_) {
+    advance();
+    return Token(type_, "\0", pos, noPos);
+}
+
+std::tuple<Token, Error> Lexer::parse_not_equals() {
     Position *pos_start = new Position(pos->inx, pos->ln, pos->col, pos->fn, pos->ftext);
     advance();
 
     if (current_char == '=') {
         advance();
-        tuple<Token, Error> res(Token("NE", "\0", (pos_start)->inx, pos->inx), Error(noPos, noPos, "", ""));
+        std::tuple<Token, Error> res(Token("NE", "\0", pos_start, pos), Error(noPos, noPos, "", ""));
         return res;
     }
 
     advance();
-    tuple<Token, Error> res(Token("\0", "\0", -2, -2), Error(pos_start, pos, "Expected Character", "'=' (after '!')\n"));
+    std::tuple<Token, Error> res(Token("\0", "\0", noPos, noPos), Error(pos_start, pos, "Expected Character", "'=' (after '!')\n"));
     return res;
 }
 
 Token Lexer::parse_equals() {
-    string tok_type = "EQ";
+    std::string tok_type = "EQ";
     Position* pos_start = pos;
     advance();
 
@@ -129,11 +106,11 @@ Token Lexer::parse_equals() {
         tok_type = "EE";
     }
 
-    return Token(tok_type, "\0", pos_start->inx, pos->inx);
+    return Token(tok_type, "\0", pos_start, pos);
 }
 
 Token Lexer::parse_less_than() {
-    string tok_type = "LT";
+    std::string tok_type = "LT";
     Position* pos_start = pos;
     advance();
 
@@ -142,11 +119,11 @@ Token Lexer::parse_less_than() {
         tok_type = "LTE";
     }
 
-    return Token(tok_type, "\0", pos_start->inx, pos->inx);
+    return Token(tok_type, "\0", pos_start, pos);
 }
 
 Token Lexer::parse_greater_than() {
-    string tok_type = "GT";
+    std::string tok_type = "GT";
     Position* pos_start = pos;
     advance();
 
@@ -155,15 +132,15 @@ Token Lexer::parse_greater_than() {
         tok_type = "GTE";
     }
 
-    return Token(tok_type, "\0", pos_start->inx, pos->inx);
+    return Token(tok_type, "\0", pos_start, pos);
 }
 
 Token Lexer::parse_number() {
-    string num_str = "";
+    std::string num_str = "";
     bool dot = false;
     Position* pos_start = pos;
 
-    while (current_char != '\0' && in(current_char, digits + ".")) {
+    while (is_digit(current_char) || current_char == '.') {
         if (current_char == '.') {
             if (dot) break;
             dot = true;
@@ -173,33 +150,30 @@ Token Lexer::parse_number() {
         advance();
     }
 
-    if (dot) {
-        return Token("FLOAT", num_str, pos_start->inx, pos->inx);
-    }
-
-    return Token("INT", num_str, pos_start->inx, pos->inx);
+    if (dot) return Token("FLOAT", num_str, pos_start, pos);
+    return Token("INT", num_str, pos_start, pos);
 }
 
 Token Lexer::parse_identifier() {
-    string id_str = "";
+    std::string id_str = "";
     Position* pos_start = pos;
 
-    while (current_char != '\0' && in(current_char, letters + digits + "_")) {
+    while (current_char != '\0' && (is_identifier(current_char) || is_digit(current_char))) {
         id_str += current_char;
         advance();
     }
 
-    string tok_type = (in(id_str, KEYWORDS)) ? "KEYWORD" : "IDENTIFIER";
-    return Token(tok_type, id_str, pos_start->inx, pos->inx);
+    std::string tok_type = (in(id_str, KEYWORDS)) ? "KEYWORD" : "IDENTIFIER";
+    return Token(tok_type, id_str, pos_start, pos);
 }
 
 Token Lexer::parse_string() {
-    string str = "";
+    std::string str = "";
     Position* pos_start = pos;
     bool escape_char = false;
     advance();
 
-    map<char, char> escape_characters;
+    std::map<char, char> escape_characters;
     escape_characters['n'] = '\n';
     escape_characters['t'] = '\t';
 
@@ -219,13 +193,21 @@ Token Lexer::parse_string() {
     }
 
     advance();
-    return Token("STRING", str, pos_start->inx, pos->inx);
+    return Token("STRING", str, pos_start, pos);
 }
 
-bool Lexer::in(char c, string str) {
-    return str.find(c) != string::npos;
+bool Lexer::is_digit(char c) {
+    return c >= '0' && c <= '9';
 }
 
-bool Lexer::in(string str, vector<string> array) {
+bool Lexer::is_identifier(char c) {
+    return (c >= 'a' && c <= 'z') || (c >= 'A' && c <= 'Z') || c == '_';
+}
+
+bool Lexer::in(char c, std::string str) {
+    return str.find(c) != std::string::npos;
+}
+
+bool Lexer::in(std::string str, std::vector<std::string> array) {
     return find(array.begin(), array.end(), str) != array.end();
 }
