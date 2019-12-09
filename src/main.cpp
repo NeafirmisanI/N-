69aa7e7f46964b23dd40f3e8f62a33ed95e1398d
-Bonfire- <40984528+BonfireScratch@users.noreply.github.com>
2019-12-09 20:56:54 +0200

    Update main.cpp

--- src/main.cpp
+++ src/main.cpp
@@ -1,48 +1,50 @@
 #include <iostream>
 #include <fstream>
+#include <sstream>
 #include <cstdio>
 #include <string>
 #include <tuple>
 #include "mainClasses.hpp"
 
-using namespace std;
-
-void run(string code) {
+void run(std::string code) {
 	Lexer lexer(code);
-	tuple<vector<Token>, Error> res = lexer.lex();
+	std::tuple<std::vector<Token>, Error> res = lexer.lex();
 
-	if (get<1>(res).details != "") {
-		cout << get<1>(res).as_string();
+	if (std::get<1>(res).details != "") {
+		std::cout << std::get<1>(res).as_string();
 		return;
 	}
 
 
-	Parser parser(get<0>(res));
+	Parser parser(std::get<0>(res));
 	parser.parse();
 }
 
 void runRepl() {
-	cout << "N# Shell v0.0.1\n";
-	cout << "Press Ctrl+C to exit\n\n";
+	std::cout << "N# Shell v0.0.1\n";
+	std::cout << "Press Ctrl+C to exit\n\n";
 	
-	string input;
+	std::string input;
 	
 	while (1) {
-		cout << "N# > ";
-		getline(cin, input);
+		std::cout << "N# > ";
+		std::getline(std::cin, input);
 		if (input == "^C") exit(0);
-		if (input.find_first_not_of(' ') == string::npos) continue;
+		if (input.find_first_not_of(' ') == std::string::npos) continue;
 		run(input);
-		cout << "\n";
+		std::cout << "\n";
 	}
 }
 
-void readFile(char* filename) {
-	string text;
-	ifstream file(filename);
+void readFile(std::string filename) {
+	std::string text;
+	std::string extension = filename.substr(filename.find_last_of("."));
+	
+	if (extension != ".nsharp") std::cout << "Invalid file extension '" << extension  << "'. Only '.nsharp' files are scanned by interpreter";	
+	std::ifstream file(filename);
 	
 	if (!file.fail()) {
-		string line;
+		std::string line;
 		
 		while (getline(file, line)) {
 			text += line.c_str();
@@ -52,18 +54,19 @@ void readFile(char* filename) {
 		text += "\n";
 		run(text);
 	} else {
-		cout << "Cannot open file " << filename;
+		std::cout << "Cannot open file " << filename;
 	}
 }
 
-int main(int argc, char *argv[]) {
+int main(int argc, char* argv[]) {
 	if (argc == 1) {
 		runRepl();
 	} else {
-		readFile(argv[1]);
+		std::string fn(argv[1]);
+		readFile(fn);
 	}
 	
-	cout << "\n";
+	std::cout << "\n";
 	getchar();
 	return 0;
 }
