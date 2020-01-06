7604f678b6cf3821b4035d7cdd3d7f8927fa6945
-Bonfire- <40984528+BonfireScratch@users.noreply.github.com>
2020-01-06 12:39:41 +0200

    Update main.cpp

--- src/main.cpp
+++ src/main.cpp
@@ -4,26 +4,41 @@
 #include <cstdio>
 #include <string>
 #include <tuple>
+#include <map>
 #include "mainClasses.hpp"
 
-void run(std::string code) {
-    Lexer lexer(code);
-    std::tuple<std::vector<Token*>, Error> res = lexer.lex();
+bool repl = true;
+SymbolTable* global_symbol_table = new SymbolTable();
 
-    if (std::get<1>(res).details != "") {
-        std::cout << std::get<1>(res).as_string();
+void make_symbol_table() {
+    global_symbol_table->set("pi", "3.1415926539");
+}
+
+void run(std::string code) {
+    Lexer* lexer = new Lexer(code);
+    std::tuple<std::vector<Token*>, Error*> res = lexer->lex();
+    delete lexer;
+
+    if (std::get<1>(res) != nullptr) {
+        std::cout << std::get<1>(res)->as_string();
         return;
     }
 
-    Parser parser(std::get<0>(res));
-    ParseResult* ast = parser.parse();
+    Parser* parser = new Parser(std::get<0>(res));
+    ParseResult* ast = parser->parse();
+    delete parser;
 
-    if (ast->error->details != "") {
+    if (ast->error != nullptr) {
         std::cout << ast->error->as_string();
         return;
     }
 
-    
+    Interpreter* interpreter = new Interpreter();
+    Context* context = new Context("<program>");
+    context->symbol_table = global_symbol_table;
+    interpreter->visit(ast->node, context);
+    delete interpreter;
+    delete context;
 }
 
 void runRepl() {
@@ -65,9 +80,12 @@ void readFile(std::string filename) {
 }
 
 int main(int argc, char* argv[]) {
+    make_symbol_table();
+
     if (argc == 1) {
         runRepl();
     } else {
+        repl = false;
         std::string fn(argv[1]);
         readFile(fn);
     }
