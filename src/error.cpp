1a25c7c835b23f5614bf90202e9e7a2e06808fba
-Bonfire- <40984528+BonfireScratch@users.noreply.github.com>
2020-02-03 20:04:38 +0200

    Update error.cpp

--- src/error.cpp
+++ src/error.cpp
@@ -12,10 +12,31 @@ std::string Error::as_string() {
     return result;
 }
 
+std::string RTError::as_string() {
+    std::string result = generate_traceback();
+    result += error_name + ": " + details;
+    result += "\n\n" + highlight(pos_start->ftext);
+    return result;
+}
+
+std::string RTError::generate_traceback() {
+    std::string result = "";
+    Position* pos = pos_start;
+    Context* ctx = context;
+
+    while (ctx != nullptr) {
+        result = "  File " + pos->fn + ", line " + std::to_string((int)(pos->ln) + 1) + ", in " + ctx->display_name + "\n" + result;
+        pos = ctx->parent_entry_pos;
+        ctx = ctx->parent;
+    }
+
+    return "Traceback (most recent call last):\n" + result;
+}
+
 std::string Error::highlight(std::string text) {
     std::string result = "";
-    int8_t idx_start = std::max(static_cast<int>(text.rfind("\n", 0, pos_start->inx)), 0);
-    int8_t idx_end = static_cast<int>(text.find("\n", idx_start + 1));
+    int8_t idx_start = std::max((int)(text.rfind("\n", 0, pos_start->inx)), 0);
+    int8_t idx_end = (int)(text.find("\n", idx_start + 1));
     if (idx_end < 0) idx_end = text.length();
     int8_t linec = pos_end->ln - pos_start->ln + 1;
 
@@ -26,7 +47,7 @@ std::string Error::highlight(std::string text) {
         result += line + '\n';
         result += std::string(col_start, ' ') + std::string(col_end - col_start, '^');
         idx_start = idx_end;
-        idx_end = static_cast<int8_t>(text.find("\n", idx_start + 1));
+        idx_end = (int8_t)(text.find("\n", idx_start + 1));
         if (idx_end < 0) idx_end = text.length();
     }
 
